04e9d0001031d8fb956684be244a0d95b0f52107
Riski Dwi <bintangmalamhari@gmail.com>
2019-09-26 12:46:29 +0700

    Add files via upload

---
+++ src/latihan9/Latihan9.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan9;
+
+/**
+ *
+ * @author dwisr
+ * NAMA      : RISKI DWI SABARIYANTO
+ * KELAS     : IF -1 
+ * NIM       : 10118026
+ * Deskripsi : Program untuk belajar variabel char atau teks
+ * 
+ */
+public class Latihan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" +newline + "char : "+ tab + "berisi "+ c);
+    }
+    
+}
